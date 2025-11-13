08f9ca5567e127b0867b44da394eeaede00b3c56
Shovezzz <sergey.s.nikishov@gmail.com>
2025-11-13 13:16:09 +0300

    filatov changes
    добавление объектов и скриптов для мишеней

--- Assets/Scripts/MultiFollowerSwitcher.cs
+++ Assets/Scripts/MultiFollowerSwitcher.cs
@@ -1,48 +1,80 @@
 using UnityEngine;
 using Dreamteck.Splines;
 
-// Этот скрипт должен висеть на том же объекте, что и оба Spline Follower
 public class MultiFollowerSwitcher : MonoBehaviour
 {
     [Header("Компоненты Spline Follower")]
-    [Tooltip("Перетащите сюда компонент Spline Follower для ОСНОВНОГО пути")]
     public SplineFollower mainFollower;
-
-    [Tooltip("Перетащите сюда компонент Spline Follower для ОБХОДНОГО пути")]
     public SplineFollower detourFollower;
 
     [Header("Точки стыковки")]
-    [Tooltip("Процент на ОСНОВНОМ сплайне, куда нужно вернуться ПОСЛЕ обхода")]
     [Range(0f, 1f)]
     public double returnPercentOnMain = 0.5;
+
+    [Header("Интерфейс выбора")]
+    public GameObject choiceInterfaceParent; 
+
+    private bool _choiceMadeThisRun = false;
+
+    void Start()
+    {
+        if (choiceInterfaceParent != null)
+        {
+            choiceInterfaceParent.SetActive(false);
+        }
+    }
+
+    public void MakeChoice(bool choseDetour)
+    {
+        if (_choiceMadeThisRun) return;
+
+        _choiceMadeThisRun = true;
+
+        if (choiceInterfaceParent != null)
+        {
+            choiceInterfaceParent.SetActive(false);
+        }
+
+        if (choseDetour)
+        {
+            Debug.Log("Игрок выбрал ОБХОДНОЙ путь");
+            SwitchToDetour();
+        }
+        else
+        {
+            Debug.Log("Игрок выбрал ОСНОВНОЙ путь");
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("SwitchToDetour"))
         {
-            if (mainFollower.enabled)
+            _choiceMadeThisRun = false; 
+            if (choiceInterfaceParent != null)
             {
-                Debug.Log("Переключаемся на обходной маршрут!");
-                mainFollower.enabled = false;
-
-                detourFollower.enabled = true;
-                detourFollower.SetPercent(0.0);
-                detourFollower.RebuildImmediate();
+                choiceInterfaceParent.SetActive(true);
             }
         }
 
-        if (other.CompareTag("SwitchBackToMain"))
+        if (other.CompareTag("SwitchBackToMain") && detourFollower.enabled)
         {
-            if (detourFollower.enabled)
-            {
-                Debug.Log("Возвращаемся на основной маршрут!");
-
-                detourFollower.enabled = false;
-
-                mainFollower.enabled = true;
-                mainFollower.SetPercent(returnPercentOnMain);
-                mainFollower.RebuildImmediate();
-            }
+            SwitchToMain();
         }
+    }
+    private void SwitchToDetour()
+    {
+        mainFollower.enabled = false;
+        detourFollower.enabled = true;
+        detourFollower.SetPercent(0.0);
+        detourFollower.RebuildImmediate();
+    }
+
+    private void SwitchToMain()
+    {
+        detourFollower.enabled = false;
+        mainFollower.enabled = true;
+        mainFollower.SetPercent(returnPercentOnMain);
+        mainFollower.RebuildImmediate();
     }
 }
---
+++ Assets/Scripts/PathChoiceReceiver.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit; 
+
+public class PathChoiceReceiver : MonoBehaviour
+{
+    public MultiFollowerSwitcher switcher;
+
+    // � ���� ���� � ���������� �������, �� ����� ���� �������� ������
+    public bool isDetourPathTarget = false;
+
+    private UnityEngine.XR.Interaction.Toolkit.Interactables.XRSimpleInteractable interactable;
+
+    void Awake()
+    {
+        interactable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRSimpleInteractable>();
+        if (switcher == null)
+        {
+            Debug.LogError("Switcher �� �������� � ���������� ������� " + gameObject.name);
+        }
+    }
+
+    void OnEnable()
+    {
+        interactable.selectEntered.AddListener(OnSelected);
+    }
+
+    void OnDisable()
+    {
+        interactable.selectEntered.RemoveListener(OnSelected);
+    }
+
+    private void OnSelected(SelectEnterEventArgs args)
+    {
+        switcher.MakeChoice(isDetourPathTarget);
+    }
+}
